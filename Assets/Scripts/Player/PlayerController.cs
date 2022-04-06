de8e61281a82604dd3cb3439dcdcb7e2cda62f86
Georgy Dolganov <mr.craaazy@gmail.com>
2022-04-06 14:17:47 +0500

    More player input refactoring

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
@@ -38,44 +39,49 @@ public class PlayerController : MonoBehaviour
 
     void Update()
     {
-        if (Input.GetKey(KeyCode.LeftShift))
-        {
-            currentSpeed = runSpeed;
-        }
-        else if (Input.GetKey(KeyCode.LeftControl))
-        {
-            currentSpeed = stepSpeed;
-        }
-        else
-        {
-            currentSpeed = normalSpeed;
-        }
-
-        rothor += Input.GetAxis("Mouse X") * senssensitivity;
-        rotver += Input.GetAxis("Mouse Y") * senssensitivity;
-
-        rotver = Mathf.Clamp(rotver, -60, 60);
-
-        Quaternion rotY = Quaternion.AngleAxis(rothor, Vector3.up);
-        Quaternion rotX = Quaternion.AngleAxis(-rotver, Vector3.right);
-
-        cam.transform.rotation = startingRotation * transform.rotation * rotX;
-        transform.rotation = startingRotation * rotY;
-
-        if (isGround)
-        {
-            ver = Input.GetAxis("Vertical") * Time.deltaTime * currentSpeed;
-            hor = Input.GetAxis("Horizontal") * Time.deltaTime * currentSpeed;
-            jump = Input.GetAxis("Jump") * Time.deltaTime * jumpspeed;
-
-            GetComponent<Rigidbody>().AddForce(transform.up * jump, ForceMode.Impulse);
-        }
-
-        transform.Translate(new Vector3(hor, 0, ver));
+        // if (Input.GetKey(KeyCode.LeftShift))
+        // {
+        //     currentSpeed = runSpeed;
+        // }
+        // else if (Input.GetKey(KeyCode.LeftControl))
+        // {
+        //     currentSpeed = stepSpeed;
+        // }
+        // else
+        // {
+        //     currentSpeed = normalSpeed;
+        // }
+        //
+        // rothor += Input.GetAxis("Mouse X") * senssensitivity;
+        // rotver += Input.GetAxis("Mouse Y") * senssensitivity;
+        //
+        // rotver = Mathf.Clamp(rotver, -60, 60);
+        //
+        // Quaternion rotY = Quaternion.AngleAxis(rothor, Vector3.up);
+        // Quaternion rotX = Quaternion.AngleAxis(-rotver, Vector3.right);
+        //
+        // cam.transform.rotation = startingRotation * transform.rotation * rotX;
+        // transform.rotation = startingRotation * rotY;
+        //
+        // if (isGround)
+        // {
+        //     ver = Input.GetAxis("Vertical") * Time.deltaTime * currentSpeed;
+        //     hor = Input.GetAxis("Horizontal") * Time.deltaTime * currentSpeed;
+        //     jump = Input.GetAxis("Jump") * Time.deltaTime * jumpspeed;
+        //
+        //     GetComponent<Rigidbody>().AddForce(transform.up * jump, ForceMode.Impulse);
+        // }
+        //
+        // transform.Translate(new Vector3(hor, 0, ver));
     }
 
-    void Move()
+    public void Move(InputAction.CallbackContext context)
     {
-        
+        Debug.Log(context);
+    }
+
+    public void Look(InputAction.CallbackContext context)
+    {
+        Debug.Log(context);
     }
 }
--- Assets/Scripts/Player/PlayerInteraction.cs
+++ Assets/Scripts/Player/PlayerInteraction.cs
@@ -21,59 +21,59 @@ public class PlayerInteraction : MonoBehaviour
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.E))
-        {
-            Ray ray = camera.ScreenPointToRay(Input.mousePosition);
-            Physics.Raycast(ray, out hit, RayDistance);
-            if (hit.rigidbody)
-            {
-                GRABI += 1;
-                switch (GRABI)
-                {
-                    case 1:
-                        Grab = true;
-                        break;
-                    case 2:
-                        Grab = false;
-                        break;
-                }
-
-                if (GRABI == 3 || Grab == false)
-                {
-                    GRABI = 0;
-                }
-            }
-        }
-
-        if (Input.GetMouseButtonDown(0) && Grab)
-        {
-            GRABI = 0;
-            Grab = false;
-            Throw = true;
-        }
-
-
-        if (Grab && hit.rigidbody)
-        {
-            hit.rigidbody.velocity = (offset.position - (hit.transform.position + hit.rigidbody.centerOfMass)) *
-                                     grabPower;
-        }
-
-        if (Throw && hit.rigidbody)
-        {
-            //ф-ция толчка
-            hit.rigidbody.velocity = camera.ScreenPointToRay(Input.mousePosition).direction * throwingSpeed;
-            Throw = false;
-        }
+        // if (Input.GetKeyDown(KeyCode.E))
+        // {
+        //     Ray ray = camera.ScreenPointToRay(Input.mousePosition);
+        //     Physics.Raycast(ray, out hit, RayDistance);
+        //     if (hit.rigidbody)
+        //     {
+        //         GRABI += 1;
+        //         switch (GRABI)
+        //         {
+        //             case 1:
+        //                 Grab = true;
+        //                 break;
+        //             case 2:
+        //                 Grab = false;
+        //                 break;
+        //         }
+        //
+        //         if (GRABI == 3 || Grab == false)
+        //         {
+        //             GRABI = 0;
+        //         }
+        //     }
+        // }
+        //
+        // if (Input.GetMouseButtonDown(0) && Grab)
+        // {
+        //     GRABI = 0;
+        //     Grab = false;
+        //     Throw = true;
+        // }
+        //
+        //
+        // if (Grab && hit.rigidbody)
+        // {
+        //     hit.rigidbody.velocity = (offset.position - (hit.transform.position + hit.rigidbody.centerOfMass)) *
+        //                              grabPower;
+        // }
+        //
+        // if (Throw && hit.rigidbody)
+        // {
+        //     //ф-ция толчка
+        //     hit.rigidbody.velocity = camera.ScreenPointToRay(Input.mousePosition).direction * throwingSpeed;
+        //     Throw = false;
+        // }
     }
 
     private void Grabb()
     {
-        Ray ray = camera.ScreenPointToRay(Input.mousePosition);
-        Physics.Raycast(ray, out hit, RayDistance);
-        if (hit.rigidbody)
-        {
-            Grab = true;
-        }
+        // Ray ray = camera.ScreenPointToRay(Input.mousePosition);
+        // Physics.Raycast(ray, out hit, RayDistance);
+        // if (hit.rigidbody)
+        // {
+        //     Grab = true;
+        // }
     }
 }
